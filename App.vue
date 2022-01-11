e4b0c19c6e53730484525882099b39d5f31c04f0
cheon seung hyun <70146658+pololover@users.noreply.github.com>
2022-01-11 17:17:42 +0900

    Component
    컴포넌트를 불러올때는 파스칼케이스로 사용은 케밥형식으로 쓰는 게 좋다. 케밥형식의 컴포넌트 정의는 링크를 타고 해당 코드를 쉽게 볼 수 있기 때문

---
+++ App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <tool-bar />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import ToolBar from './components/ToolBar.vue'
+export default {
+  name: 'App',
+  components : {
+    ToolBar,
+  },
+  methods: {
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
