080706f6e6e7fd26fec7439d9494e9d682e5945c
cheon seung hyun <70146658+pololover@users.noreply.github.com>
2022-01-11 18:44:17 +0900

    vue lifecycle
    created : 가상Dom이 그려진 상태
    mounted : Dom이 그려진 상태.  
    mounted에서 데이터를 수정또는 불러오면 Dom이 다시 그려지기 때문에 이러한 부분은 created에서 활용

---
+++ AskView.vue
@@ -0,0 +1,25 @@
+<template lang="">
+  <div>
+    asks
+    <div v-for="ask in asks">{{ask}}</div>
+  </div>
+</template>
+<script>
+import {fetchAskList} from '../api/index.js'
+export default {
+  data() {
+    return {
+      asks : []
+    }
+  },
+  //mounted에 fetch를 넣게되면 이미 created단계가 지난 data를 Dom에 그리고 data를 수정함으로써 또 다시 그리기때문에 비효율적 
+  created() {
+    fetchAskList()
+      .then(res => this.asks = res.data)
+      .catch(err => console.log(err))
+  }
+}
+</script>
+<style lang="">
+  
+</style>
