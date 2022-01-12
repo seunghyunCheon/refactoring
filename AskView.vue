235ed4f56a0b3848a7aed83a4e91e80770053793
cheon seung hyun <70146658+pololover@users.noreply.github.com>
2022-01-12 16:05:40 +0900

    mapState, mapGetter
    각각 vuex의 state를 mapping한 상태로 불러오거나 mapping해주는 라이브러리

--- AskView.vue
+++ AskView.vue
@@ -1,22 +1,25 @@
 <template lang="">
   <div>
     asks
-    <div v-for="ask in asks">{{ask}}</div>
+    <div v-for="item in fetchedAsk">{{item}}</div>
   </div>
 </template>
 <script>
-import {fetchAskList} from '../api/index.js'
+import {mapGetters} from 'vuex' //vuex에서 제공하는 map을 적용한 상태의 state를 불러오는 함수.
+
 export default {
-  data() {
-    return {
-      asks : []
-    }
+  computed : {
+
+    ...mapGetters([
+      'fetchedAsk'
+    ])
+    // ask(){
+    //   return this.$store.state.ask
+    // }
   },
   //mounted에 fetch를 넣게되면 이미 created단계가 지난 data를 Dom에 그리고 data를 수정함으로써 또 다시 그리기때문에 비효율적 
   created() {
-    fetchAskList()
-      .then(res => this.asks = res.data)
-      .catch(err => console.log(err))
+    this.$store.dispatch('FETCH_ASK');
   }
 }
 </script>
