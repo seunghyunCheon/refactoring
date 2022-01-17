bc41f40dbc0c9be1960f7420a537c7af9f0a44eb
cheon seung hyun <70146658+pololover@users.noreply.github.com>
2022-01-17 17:58:39 +0900

    navigation guard

--- index.js
+++ index.js
@@ -1,26 +1,73 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-import mutations from './mutation.js'
-import actions from './actions.js'
-Vue.use(Vuex);
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import UserView from '../Views/UserView.vue'
+import ItemView from '../Views/ItemView.vue'
+import createListView from '../Views/CreateListView.js'
+import { store } from '../store/index.js'
+import bus from '../utils/bus.js'
+Vue.use(VueRouter)
 
-export const store = new Vuex.Store({
-  state: {
-    news: [],
-    ask: [],
-    jobs: [],
-    user: {},
-    item : {},
-  },
-  getters: {
-    fetchedAsk(state) {
-      return state.ask;
+export const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      redirect: '/news',
     },
-    fetchedItem(state) {
-      return state.item
+    {
+      path: '/news',
+      name: 'news',
+      beforeEnter: (to, from, next) => {
+        bus.$emit('start:spinner');
+        store.dispatch('FETCH_LIST', to.name)
+          .then(() => {
+            
+              bus.$emit('end:spinner');
+            
+            }) //dispatch : vuex의 action을 실행
+          .catch((err) => console.log(err))
+        next();
+      },
+      // component: NewsView
+      component: createListView('NewsView'),
+    },
+    {
+      path: '/ask',
+      name: 'ask',
+      beforeEnter: (to, from, next) => {
+        bus.$emit('start:spinner');
+        store.dispatch('FETCH_LIST', to.name)
+          .then(() => {
+            bus.$emit('end:spinner');
+          }) //dispatch : vuex의 action을 실행
+          .catch((err) => console.log(err))
+        next();
+      },
+      // component : AskView
+      component: createListView('AskView'),
+    },
+    {
+      path: '/jobs',
+      name: 'jobs',
+      beforeEnter: (to, from, next) => {
+        bus.$emit('start:spinner');
+        store.dispatch('FETCH_LIST', to.name)
+          .then(() => {
+              bus.$emit('end:spinner');
+          }) //dispatch : vuex의 action을 실행
+          .catch((err) => console.log(err))
+        next();
+      },
+      // component : JobsView
+      component : createListView('JobsView'),
+    },
+    {
+      path: '/user/:id',
+      component : UserView,
+    },
+    {
+      path: '/item/:id',
+      component : ItemView
     }
-  }
-  ,
-  mutations: mutations,
-  actions: actions,
+  ],
 })
